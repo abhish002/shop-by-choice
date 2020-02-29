5df616f8a9f3715da327bc7c898bbd184e8d6906
Abhish Abraham <abhish002@gmail.com>
2020-02-29 15:53:25 -0500

    Added header component and updated header links.

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
 import React from 'react';
 import './App.css';
 import Homepage from './pages/homepage/homepage.component';
-import { Route } from 'react-router-dom';
+import ShopPage from './pages/shop/shop.component';
+import { Route, Switch } from 'react-router-dom';
+import Header from '../src/components/header/header.component';
 
 const HatsPage = () => (
   <div>
@@ -12,12 +14,17 @@ const HatsPage = () => (
 function App() {
   return (
     <div>
-      <Route exact path='/' component={Homepage} />
-      <Route exact path='/shop/hats' component={HatsPage} />
-      <Route exact path='/shop/jackets' component={HatsPage} />
-      <Route exact path='/shop/sneakers' component={HatsPage} />
-      <Route exact path='/shop/womens' component={HatsPage} />
-      <Route exact path='/shop/mens' component={HatsPage} />
+      <Header />
+      <Switch>
+        <Route exact path='/' component={Homepage} />
+        <Route exact path='/shop' component={ShopPage} />
+        <Route exact path='/shop/hats' component={HatsPage} />
+        <Route exact path='/shop/jackets' component={HatsPage} />
+        <Route exact path='/shop/sneakers' component={HatsPage} />
+        <Route exact path='/shop/womens' component={HatsPage} />
+        <Route exact path='/shop/mens' component={HatsPage} />
+      </Switch>
+
     </div>
   );
 }
