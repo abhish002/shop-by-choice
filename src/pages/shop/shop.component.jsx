97db45a84f2a3c6e16dec77f315876fdb56565f7
Abhish Abraham <abhish002@gmail.com>
2020-03-26 23:09:37 -0400

    Moved shop state to shop reducer.

--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -1,26 +1,29 @@
-import React, { Component } from 'react';
+import React from 'react';
 import './shop.styles.scss';
 import PreviewCollections from '../../components/preview-collections/preview-collections.component';
-import SHOP_DATA from './shop.data';
 
-export default class ShopPage extends Component {
-  constructor() {
-    super();
-    this.state = {
-      collections: SHOP_DATA
-    };
-  }
+import { connect } from 'react-redux';
+import { createStructuredSelector } from 'reselect';
 
-  render() {
-    const { collections } = this.state;
-    const shops = collections.map(({ id, ...otherCollectionsProps }) => (
-      <PreviewCollections key={id} {...otherCollectionsProps} />
-    ));
-    return (
-      <div className='shop-page'>
-        {shops}
-      </div>
-    )
-  }
-}
+import { selectCollections } from '../../selectors/shop/shop.selector';
+
+function Shop({ collections }) {
+  const shops = collections.map(({ id, ...otherCollectionsProps }) => (
+    <PreviewCollections key={id} {...otherCollectionsProps} />
+  ));
+  return (
+    <div className='shop-page'>
+      {shops}
+    </div>
+  )
+};
+
+const mapStateToProps = createStructuredSelector({
+  collections: selectCollections,
+});
+
+export default connect(mapStateToProps)(Shop);
+
+
+
 
--- src/reducers/root-reducer.reducer.js
+++ src/reducers/root-reducer.reducer.js
@@ -5,17 +5,19 @@ import storage from 'redux-persist/lib/storage';
 import userReducer from './user.reducer';
 import cartReducer from './cart.reducer';
 import directoryReducer from './directory.reducer';
+import shopReducer from './shop.reducer';
 
 const persistConfig = {
   key: 'root',
   storage,
-  whitelist: ['cart', 'directory']
+  whitelist: ['cart', 'directory', 'shop']
 };
 
 const rootReducer = combineReducers({
   user: userReducer,
   cart: cartReducer,
   directory: directoryReducer,
+  shop: shopReducer,
 });
 
 export default persistReducer(persistConfig, rootReducer);
